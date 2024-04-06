64b6e42fe03c37e4148fb0493676f8df480c43f8
serhatacarhello <serhat.acar.tr@gmail.com>
2024-04-06 17:49:04 +0300

    create schemaTypes for sanity.io

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,13 +2,14 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
+import { sanityDataset, sanityProjectId } from '@/app/constants'
 
 export default defineConfig({
   name: 'default',
   title: 'hotel-management',
 
-  projectId: 'uol9eng8',
-  dataset: 'production',
+  projectId:sanityProjectId ,
+  dataset: sanityDataset,
   basePath:"/studio",
 
   plugins: [structureTool(), visionTool()],
---
+++ src/app/constants/index.ts
@@ -0,0 +1,16 @@
+export const sanityProjectId = process.env
+  .NEXT_PUBLIC_SANITY_PROJECT_ID! as string;
+export const sanityDataset = process.env.NEXT_PUBLIC_SANITY_DATASET! as string;
+export const sanityApiVersion = process.env
+  .NEXT_PUBLIC_SANITY_API_VERSION! as string;
+
+export const githubClientId = process.env
+  .NEXT_PUBLIC_GITHUB_CLIENT_ID! as string;
+export const githubClientSecret = process.env
+  .NEXT_PUBLIC_GITHUB_CLIENT_SECRET! as string;
+export const googleClientId = process.env
+  .NEXT_PUBLIC_GOOGLE_CLIENT_ID! as string;
+export const googleClientSecret = process.env
+  .NEXT_PUBLIC_GOOGLE_CLIENT_SECRET! as string;
+
+// SANITY_API_READ_TOKEN=
